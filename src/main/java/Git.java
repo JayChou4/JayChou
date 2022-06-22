9624b7684c5754015b7ce9a52cc5f60edf75eb08
WangTaiWei <WTW@qq.com>
2022-06-22 14:15:11 +0800

    one

---
+++ src/main/java/Git.java
@@ -0,0 +1,8 @@
+public class Git {
+    public static void main(String[] args){
+        System.out.println("Git1");
+        System.out.println("Git2");
+        System.out.println("Git3");
+        System.out.println("Git4");
+    }
+}
